bd1082a3db9892c0c52c158dfbc32aabed0206a4
Berteun Damman <berteun@gmail.com>
2019-12-01 18:30:41 +0100

    Add day 1, part 2 solution in Rust

--- day01_rs/src/main.rs
+++ day01_rs/src/main.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::fs::File;
 use std::io::{prelude::*, BufReader};
 
@@ -13,7 +14,7 @@ fn read_input() -> Vec<u32> {
     return vec;
 }
 
-fn compute_fuel(masses: Vec<u32>) -> u32 {
+fn compute_fuel(masses: &[u32]) -> u32 {
     let mut fuel = 0;
     for mass in masses {
         fuel += mass / 3 - 2;
@@ -21,8 +22,28 @@ fn compute_fuel(masses: Vec<u32>) -> u32 {
     return fuel;
 }
 
+fn fuel_for_fuel(mut fuel: u32) -> u32 {
+    let mut fuel_for_fuel = 0;
+    while fuel > 8 {
+        fuel = cmp::max((fuel / 3) - 2, 0);
+        fuel_for_fuel += fuel;
+    }
+    return fuel_for_fuel;
+}
+
+fn compute_fuel_with_fuel(masses: &[u32]) -> u32 {
+    let mut fuel = 0;
+    for mass in masses {
+        let base_fuel = mass / 3 - 2;
+        fuel += base_fuel + fuel_for_fuel(base_fuel)
+    }
+    return fuel;
+}
+
 fn main() {
     let mass_list = read_input();
-    let fuel = compute_fuel(mass_list);
+    let fuel = compute_fuel(&mass_list);
     println!("Part 01: {}", fuel);
+    let fuel_with_fuel = compute_fuel_with_fuel(&mass_list);
+    println!("Part 02: {}", fuel_with_fuel);
 }
