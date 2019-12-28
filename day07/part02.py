59e4bf418fcb2beb67285e55df4eb0a9cba4f8ce
Berteun Damman <berteun@gmail.com>
2019-12-28 16:32:53 +0100

    Update day 7, part 2 solution.

--- day07/part02.py
+++ day07/part02.py
@@ -1,240 +1,47 @@
 import itertools
-
-class Argument:
-    def __init__(self, mode):
-        self.mode = mode
-
-    def get(self, array, val):
-        if self.mode == 0:
-            return array[val]
-        else:
-            return val
-
-class Add:
-    arguments = 3
-    opcode = 1
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
-
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        array[vals[2]] = r[0] + r[1]
-        machine.pos += self.arguments
-
-class Mul:
-    arguments = 3
-    opcode = 2
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
-
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        array[vals[2]] = r[0] * r[1]
-        machine.pos += self.arguments
-
-class Input:
-    arguments = 1
-    opcode = 3
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
-
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        array[vals[0]] = machine.get_input()
-        machine.pos += self.arguments
+import sys
+sys.path.append('..')
+from intcode import IntCodeMachine, open_file
 
 
-class Output:
-    arguments = 1
-    opcode = 4
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
+class MachineIO:
+    def __init__(self, machine_queues, n, phase):
+        self.queues = machine_queues
+        self.output = [phase]
+        self.n = n
+        self.max_output = 0
 
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        machine.output(r[0])
-        machine.pos += self.arguments
+    def oninput(self):
+        return self.queues[(self.n - 1) % 5].output.pop(0)
 
-class JumpIfTrue:
-    arguments = 2
-    opcode = 5
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
-
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        if (r[0] != 0):
-            machine.pos = r[1]
-        else:
-            machine.pos += self.arguments
-
-class JumpIfFalse:
-    arguments = 2
-    opcode = 5
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
-
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        if (r[0] == 0):
-            machine.pos = r[1]
-        else:
-            machine.pos += self.arguments
-
-class LessThan:
-    arguments = 3
-    opcode = 5
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
-
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        array[vals[2]] = int(r[0] < r[1])
-        machine.pos += self.arguments
-
-class Equals:
-    arguments = 3
-    opcode = 5
-    def __init__(self, machine, *args):
-        assert(len(args) == self.arguments)
-        self.machine = machine
-        self.args = args
-
-    def eval(self):
-        machine = self.machine
-        vals = machine.get_args(self.arguments)
-        array = machine.instructions
-        r = [self.args[i].get(array, vals[i]) for i in range(self.arguments)]
-        array[vals[2]] = int(r[0] == r[1])
-        machine.pos += self.arguments
-
-opcodes = {
-        1: Add,
-        2: Mul,
-        3: Input,
-        4: Output,
-        5: JumpIfTrue,
-        6: JumpIfFalse,
-        7: LessThan,
-        8: Equals,
-}
-
-class Machine:
-    def __init__(self, instructions):
-        self.instructions = instructions
-        self.pos = 0
-        self.halted = False
-        self.output_value = None
-
-    def get_args(self, n):
-        return self.instructions[self.pos:self.pos + n]
-
-    def decode(self, code):
-        opcode = code % 100;
-        code //= 100;
-        args = []
-        for n in range(opcodes[opcode].arguments):        
-            mode = code % 10
-            args.append(Argument(mode))
-            code //= 10
-        return opcodes[opcode](self, *args)
-
-    def step(self):
-        if not self.halted:
-            opcode = self.decode(self.instructions[self.pos])
-            self.pos += 1
-            try:
-                opcode.eval()
-            except:
-                self.halted = True
-                return
-            self.halted = (self.instructions[self.pos] == 99)
-
-    
-    def get_input(self):
-        return self.input.pop(0)
-    
-    def output(self, value):
-        self.output_value = value
-
-    def run(self, initial_input):
-        self.input = initial_input[:]
-        while not self.halted:
-            self.step()
-            if self.output_value:
-                next_input = yield self.output_value
-                self.output_value = None
-                self.input.append(next_input)
-        yield None
-
-def read_input():
-    f = open("input_day07.txt")
-    #f = open("example03.txt")
-    l = [int(n) for n in f.readline().strip().split(",")]
-    return l
+    def onoutput(self, val):
+        self.max_output = max(val, self.max_output)
+        self.output.append(val)
 
 def run():
+    instructions = open_file("input_day07.txt")
+
     best = 0
-    best_perm = None
-    input_list = read_input()
     for perm in itertools.permutations([5,6,7,8,9]):
         machines = []
-        generators = []
-        for _ in range(5):
-            machines.append(Machine(input_list[:]))
+        machine_io = []
+        for n in range(5):
+            io = MachineIO(machine_io, n, perm[n])
+            machine_io.append(io)
+            machines.append(IntCodeMachine(instructions.copy(), io.oninput, io.onoutput))
 
-        prev = 0
-        for i in range(5):
-            generators.append(machines[i].run([perm[i], prev]))
-            prev = next(generators[i])
+        # Kick off signal, since the last machine feeds into the first, we put it there
+        machine_io[-1].output.append(0)
 
-        i = 0
         while not (all(machine.halted for machine in machines)):
-            if not machines[i].halted:
-                prev = generators[i].send(prev);
+            for machine, io in zip(machines, machine_io):
+                while not machine.halted:
+                    machine.step()
+                    if io.output:
+                        break;
 
-            if i == 4 and prev > best:
-                best = prev
-                best_perm = perm
-
-            i = (i + 1) % 5
-
-    print("best", best, best_perm)
+        best = max(machine_io[-1].max_output, best)
+    print(best)
 
 if __name__ == '__main__':
     run()
